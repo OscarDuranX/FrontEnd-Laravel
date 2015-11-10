0caf178424d2a6c36b5390a9f2e0278b412b8b08
OscarDuranX <oscarduran@iesebre.com>
2015-11-10 18:24:07 +0100

    prime commit

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by oscar on 10/11/15.
+ */
+console.debug("xivaxiva");
+$("#title").html('Prova programa JS HELLO WORLD');
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel FrontEnd El EXEMPLE FINAL!</title>
+        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script src="/js/main.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div id="title">Prova HOLA</div>
+            </div>
+        </div>
+    </body>
+</html>
